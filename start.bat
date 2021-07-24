8c25381d434f1f949812620d8f3585f8fd00bf69
vivigwen <85069919+vivigwen@users.noreply.github.com>
2021-07-24 21:41:06 +0700

    Update start.bat
    update security

--- start.bat
+++ start.bat
@@ -14,6 +14,6 @@ echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: ViViGweN
+echo Password: ~ViViGweN~
 echo Silahkan Login Sekarang
 ping -n 10 127.0.0.1 >nul
